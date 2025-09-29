b5f2bf41aba2efa0abc4181c87f1d736a52e61a3
Makar <makarsidorevic@gmail.com>
2025-09-29 14:42:28 +0300

    First prak t4

--- src/prak_1/task3.java
+++ src/prak_1/task3.java
@@ -2,8 +2,8 @@ package prak_1;
 
 public class task3 {
     public static void main(String[] argc) {
-        for (int i = 0; i < argc.length; i++) {
-            System.out.println(argc[i]);
+        for (String s : argc) {
+            System.out.println(s);
         }
     }
 }
---
+++ src/prak_1/task4.java
@@ -0,0 +1,9 @@
+package prak_1;
+
+public class task4 {
+    public static void main(String[] argc) {
+        for (int n = 1; n <= 10; n++) {
+            System.out.println(1.0 / n);
+        }
+    }
+}
